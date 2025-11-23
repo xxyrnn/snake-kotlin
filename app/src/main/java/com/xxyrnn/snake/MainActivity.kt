f5bef6d69b55730016b71a79f593262d70f4f9b1
xxyrnn <xyrnproject@gmail.com>
2025-11-23 04:55:03 +0100

    cleaned unused import

--- app/src/main/java/com/xxyrnn/snake/MainActivity.kt
+++ app/src/main/java/com/xxyrnn/snake/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
